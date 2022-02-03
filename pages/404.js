95d472f5f977bdf9a12e2e9d918e3d9852dc0bef
João Pedro Matias <joao.matias@rebase.com.br>
2022-02-03 10:05:12 -0300

    Adicionando 404

---
+++ pages/404.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import useRouter from 'next/router';
+import router from 'next/router';
+
+const NotFound = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/');
+		}, 4000);
+	}, []);
+
+	return (
+		<div className='not-found'>
+			<h1>404</h1>
+			<h2>Ooopss! Página não foi encontrada :(</h2>
+			<p>
+				Rediricionando para a <Link href='/'>página inicial</Link>
+			</p>
+			<style jsx>
+				{`
+					.not-found {
+						background: #fff;
+						padding: 30px;
+						box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+						transform: rotateZ(-1deg);
+					}
+					h1 {
+						font-size: 3em;
+					}
+				`}
+			</style>
+		</div>
+	);
+};
+
+export default NotFound;
